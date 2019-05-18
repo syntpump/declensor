6038bd4888fce313862ee9b61205de8216ec4509
Paul Lynn <lynnporu@gmail.com>
2019-05-18 18:56:08 +0300

    Create PyPI structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="dclua",
+    version="1.0",
+    description="Library for word declensions",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/syntpump/declensor",
+    license="MIT License",
+    author="Syntpump",
+    author_email="lynnporu@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License"
+    ],
+    keywords="nlp",
+    packages=setuptools.find_packages(),
+    python_requires=">3",
+    project_urls={
+        "Syntpump on GitHub": "https://github.com/syntpump"
+    }
+)
