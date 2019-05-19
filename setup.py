ab9a69b5fc570aa73216f70b724c07e9afe6dfc5
Paul Lynn <lynnporu@gmail.com>
2019-05-19 11:25:41 +0300

    Increase version, add __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="dclua",
-    version="1.0",
+    version="1.1",
     description="Library for word declensions",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
